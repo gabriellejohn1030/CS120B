8e63a6c5f3e0e9595c7f70badb59a64c6e09290f
Gabrielle John <gjohn010@wch136-24.cs.ucr.edu>
2020-10-20 16:53:13 -0700

    modified part2

--- Lab3_BitManip/turnin/gjohn010_lab3_part2.c
+++ Lab3_BitManip/turnin/gjohn010_lab3_part2.c
@@ -15,6 +15,9 @@
 unsigned char GetBit(unsigned char x, unsigned char k) {
 	return ((x & (0x01 << k)) != 0);
 }
+unsigned char SetBit(unsigned char x, unsigned char y, unsigned char z) {
+	return (z ? | (0x01 << y) : x & ~(0x01 << y));
+}
 
 int main(void) {
     /* Insert DDR and PORT initializations */
@@ -22,105 +25,49 @@ int main(void) {
 	PORTA = 0xFF;
 	DDRC = 0xFF;
 	PORTC = 0x00;
-//	DDRB = 0x00;
-//	PORTB = 0xFF;
+	unsigned char temp1 = 0x00;
+	unsigned char temp2 = 0xFF;
+	unsigned char temp3 = 0x00;
+
     /* Insert your solution below */
-  /*  while (1) {
-	unsigned char tempA = PINA;
-	unsigned tempB = PINB;
-	unsigned char counter = 0;
-	unsigned char i;
-	for (i = 0; i < 8; ++i) {
-		if(GetBit(tempA, i)) {
-			++counter;
-		}
-	}
-	for (i = 0; i < 8; ++i) {
-		if(GetBit(tempB, i)) {
-			++counter;
-		}
-	}
-	PORTC = counter;
-
-
-    }
-*/
-
  while(1) {
-/*	unsigned char fuelLevel = PINA & 0x0F;
-	unsigned char light = 0;
-	unsigned char lowFuel = 0;
-	
-	unsigned char key = GetBit(PINA, 4);
-	unsigned char seat = GetBit(PINA,5);
-	unsigned char seatbelt = GetBit(PINA, 6);
-	unsigned char fastenbelt = 0;
+	temp1 = PINA;
+        			 
+		if (temp1 <= 0x0F && temp1 >= 0x0D) // 13-15 --> PC5-PC0 
+		{
+			temp2 = 0xFF;
+		}
+		if(temp1 <= 0x0C && temp1 >= 0x0A) // 10-12 --> PC5-PC1
+		{
+			temp2 = 0xFF << 1;
+		}
+		if (temp1 <= 0x09 && temp1 >= 0x07) // 7-9 --> PC5-PC2
+		{
+			temp2 = 0xFF << 2;	
+		}
+		if (temp1 <= 0x06 && temp1 >= 0x05) // 5-6 --> PC5-PC3
+		{
+			temp2 = 0xFF << 3;
+		}
+		if (temp1 <= 0x04 && temp1 >= 0x03) // 3-4 --> PC5-PC4
+		{
+			temp2 = 0xFF << 4;
+		}
+		if (temp1 <= 0x02 && temp1 >= 0x01) // 1-2 --> PC5
+        	{
+           		temp2 = 0xFF << 5;
+       	}
+		if (temp1 == 0x00) { // 0 --> all off
+           		temp2 = 0xFF << 6;
+        	}
+		
+		temp3 = temp2 & 0x3F;
+		
+		if (temp1 <= 0x04) // if PA <= 4, set PC6
+        	{
+           		temp3 = temp3 + 0x40;
+        	}
 
-	if (fuelLevel == 2 || fuelLevel == 1) {
-		light = 0x20;
-		lowFuel = 0x40;
-	}
-	if (fuelLevel == 3 || fuelLevel == 4) {
-		light = 0x30;
-		lowFuel = 0x40;
-	}
-	if (fuelLevel == 5 || fuelLevel == 6) {
-		light = 0x38;
-	}
-	if (fuelLevel == 13 || fuelLevel == 14 || fuelLevel == 15) {
-		light 0x3F;
-	}
-	if (fuelLevel == 10 || fuelLevel == 11 || fuelLevel == 12) {
-		light = 0x3E;
-	}
-	if (fuelLevel == 7 || fuelLevel == 8 || fuelLevel == 9) {
-		light = 0x3C;
-	}
-	if (seatbelt) {
-		fastenbelt = 0x00;
-	}
-	else {
-		if (seat && key) {
-			fastenbelt = 0x80;
-		}
-		else {
-			fastenbelt = 0x00;
-		}
-	}
-		
-	PORTC = light | lowFuel | fastenbelt;
-
- }
-    return 0;
-*/
- unsigned char fuelLevel = PINA & 0x0F;
- unsigned char light = 0;
- unsigned char lowFuel = 0;
-		
-		if (fuelLevel == 2 || fuelLevel == 1) {
-			light = 0x20;
-			lowFuel = 0x40;
-		}
-		if (fuelLevel == 3 || fuelLevel == 4) {
-			light = 0x30;
-			lowFuel = 0x40;
-		}
-		if (fuelLevel == 5 || fuelLevel == 6) {
-			light = 0x38;
-		
-		}
-		if (fuelLevel == 13 || fuelLevel == 14 || fuelLevel == 15) {
-			light = 0x3F;
-		}
-		if (fuelLevel == 10 || fuelLevel == 11 || fuelLevel == 12) {
-			light = 0x3E;
-		}
-		if (fuelLevel ==7 || fuelLevel == 8 || fuelLevel == 9) {
-			light = 0x3C;
-		}
-		
-
-		PORTC = light | lowFuel;
-	}	
-   return 0;
+		PORTC = temp3;
+    }
 }
