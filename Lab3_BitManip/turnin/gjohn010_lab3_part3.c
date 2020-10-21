e6be44598c9636798e3e754fcef499e6287dfef3
Gabrielle John <gjohn010@wch136-24.cs.ucr.edu>
2020-10-20 21:30:42 -0700

    modified lab3

--- Lab3_BitManip/turnin/gjohn010_lab3_part3.c
+++ Lab3_BitManip/turnin/gjohn010_lab3_part3.c
@@ -1,7 +1,7 @@
 /*	Author: gjohn010
  *  Partner(s) Name: 
- *	Lab Section:023
- *	Assignment: Lab #3  Exercise #2
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -22,91 +22,77 @@ int main(void) {
 	PORTA = 0xFF;
 	DDRC = 0xFF;
 	PORTC = 0x00;
-
-
+//	DDRB = 0x00;
+//	PORTB = 0xFF;
     /* Insert your solution below */
+  /*  while (1) {
+	unsigned char tempA = PINA;
+	unsigned tempB = PINB;
+	unsigned char counter = 0;
+	unsigned char i;
+	for (i = 0; i < 8; ++i) {
+		if(GetBit(tempA, i)) {
+			++counter;
+		}
+	}
+	for (i = 0; i < 8; ++i) {
+		if(GetBit(tempB, i)) {
+			++counter;
+		}
+	}
+	PORTC = counter;
+
+
+    }
+*/
+
  while(1) {
 	unsigned char fuelLevel = PINA & 0x0F;
 	unsigned char light = 0;
 	unsigned char lowFuel = 0;
-
+	
 	unsigned char key = GetBit(PINA, 4);
-	unsigned char seat = GetBit(PINA, 5);
+	unsigned char seat = GetBit(PINA,5);
 	unsigned char seatbelt = GetBit(PINA, 6);
 	unsigned char fastenbelt = 0;
 	
-		if (fuelLevel == 2 || fuelLevel == 1) {
-			light = 0x20;
-			lowFuel = 0x40;
-		}
-		if (fuelLevel == 3 || fuelLevel == 4) {
-			light = 0x30;
-			lowFuel = 0x40;
-		}
-		if (fuelLevel == 5 || fuelLevel == 6) {
-			light = 0x38;
-		
-		}
-		if (fuelLevel == 13 || fuelLevel == 14 || fuelLevel == 15) {
-			light = 0x3F;
-		}
-		if (fuelLevel == 10 || fuelLevel == 11 || fuelLevel == 12) {
-			light = 0x3E;
-		}
-		if (fuelLevel ==7 || fuelLevel == 8 || fuelLevel == 9) {
-			light = 0x3C;
-		}
-
-		if (seatbelt) {
-			fastenbelt = 0x00;
+	if (fuelLevel == 0) {
+		lowFuel = 0x40;
+	}
+	if (fuelLevel == 2 || fuelLevel == 1) {
+		light = 0x20;
+		lowFuel = 0x40;
+	}
+	if (fuelLevel == 3 || fuelLevel == 4) {
+		light = 0x30;
+		lowFuel = 0x40;
+	}
+	if (fuelLevel == 5 || fuelLevel == 6) {
+		light = 0x38;
+	}
+	if (fuelLevel == 13 || fuelLevel == 14 || fuelLevel == 15) {
+		light 0x3F;
+	}
+	if (fuelLevel == 10 || fuelLevel == 11 || fuelLevel == 12) {
+		light = 0x3E;
+	}
+	if (fuelLevel == 7 || fuelLevel == 8 || fuelLevel == 9) {
+		light = 0x3C;
+	}
+	if (seatbelt) {
+		fastenbelt = 0x00;
+	}
+	else {
+		if (seat && key) {
+			fastenbelt = 0x80;
 		}
 		else {
-			if (seat && key) {
-				fastenbelt = 0x80;
-			}
-			else {
-				fastenbelt = 0x00;
-			}
+			fastenbelt = 0x00;
 		}
-
-
-		PORTC = light | lowFuel | fastenbelt;
-	}	
-   return 0;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	}
 		
+	PORTC = light | lowFuel | fastenbelt;
 
-		
-
-
-
-
-
-
-
+ }
+    return 0;
 }
