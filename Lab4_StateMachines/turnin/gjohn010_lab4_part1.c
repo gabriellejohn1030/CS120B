7f1857d3ee0ffa4af41d6c8b3520229e84442f20
Gabrielle John <gjohn010@wch136-24.cs.ucr.edu>
2021-10-10 20:19:20 -0700

    completed lab 4 part 1

--- Lab4_StateMachines/turnin/gjohn010_lab4_part1.c
+++ Lab4_StateMachines/turnin/gjohn010_lab4_part1.c
@@ -1,6 +1,6 @@
 /*	Author: gjohn010
  *  Partner(s) Name: 
- *	Lab Section: 023
+ *	Lab Section: 022
  *	Assignment: Lab #4  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
@@ -12,254 +12,63 @@
 #include "simAVRHeader.h"
 #endif
 
-enum my_states { start, s0, s1 } my_state;
+enum States { Start, PB0, PB1 } state;
+unsigned char tmp1 = 0x00;
+unsigned char tmp2 = 0x00;
 
-void tick() {
-	switch (my_state) { //transitions
-		case start:
-			my_state = s0;		
+
+void Tick() {
+	switch(state) {
+		case Start:
+			state = PB0;
 			break;
-		case s0:
-			if(!PINA) {
-				my_state = s0;
+		case PB0:
+			if (tmp1 == 0x01) {
+				state = PB1;
 			}
 			else {
-				my_state = s1;
+				state = PB0;
 			}
 			break;
-		case s1:
-			if(PINA) {
-				my_state = s0;
+		case PB1: 
+			if (tmp1 == 0x01) {
+				state = PB0;
 			}
 			else {
-				my_state = s1;
+				state = PB1;
 			}
 			break;
 		default:
-			my_state = start;
-			break;
+		{
+			break;}
 	}
+	switch(state) {
+		case Start:
+		{
+			break;}
 	
-	switch (my_state) {
-		case s0:
-			PORTB = 0x01;
+		case PB0:	
+			tmp2 = 0x01;
+			PORTB = tempB;
 			break;
-		case s1:
-			PORTB = 0x02;
+		case PB1:
+			tmp2 = 0x02;
+			PORTB = tempB;
 			break;
 		default:
-			break;
-
+		{
+			break;}
 	}
 }
-
-
-int main(void) {
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-      	PORTB = 0x01;
-	my_state = start;
-
-
-	while(1) {
-		tick();
-	}
-
-
-
-
-	
-return 0;
+int main() {
+    /* Insert DDR and PORT initializations */
+	state = Start;
+	DDRB = 0xFF;
+	PORTB = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	tempA = PINA;
+	Tick();
+    }
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
