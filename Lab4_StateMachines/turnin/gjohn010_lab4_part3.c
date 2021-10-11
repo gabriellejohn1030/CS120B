b0bb603f71bd605ecc82b8a59da0bd07a2a735ae
Gabrielle John <gjohn010@wch136-24.cs.ucr.edu>
2021-10-10 22:24:36 -0700

    completed lab 4 part 3

--- Lab4_StateMachines/turnin/gjohn010_lab4_part3.c
+++ Lab4_StateMachines/turnin/gjohn010_lab4_part3.c
@@ -1,7 +1,7 @@
 /*	Author: gjohn010
  *  Partner(s) Name: 
- *	Lab Section: 023
- *	Assignment: Lab #4  Exercise #1
+ *	Lab Section: 022
+ *	Assignment: Lab #4  Exercise #3
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,281 +12,61 @@
 #include "simAVRHeader.h"
 #endif
 
-enum States { START, INIT, S_HASH, S_Y } state;
-unsigned char tmpA = 0x00, tmpB = 0x00;
-    
+enum States { Start, door_state, unlock } state;
+unsigned char tmpA - 0x00;
+
 void Tick() {
-    switch(state) { 
-        case START:
-            state = INIT;
-            break;
-        case INIT:
-            if (tmpA == 0x04) {
-                state = S_HASH;
-            }
-            else {
-                state = INIT;
-            }
-            break;
-        case S_HASH:
-            if (tmpA == 0x02) {
-                state = S_Y;
-            }
-            else if (tmpA & 0x04 || ~tmpA & 0x02 || tmpA & 0x01 || tmpA & 0x80) {
-                state = INIT;
-            }
-            break;
-        case S_Y:
-            if (tmpA & 0x80) {
-                state = INIT;
-            }
-            break;
-		default:
+	switch(state) {
+		case Start:
+			state = Start;
 			break;
-    }
-    switch(state) {
-        case INIT:
-            tmpB = 0x00;
-            PORTB = tmpB;
-            break;
-        case S_HASH:
-            break;
-        case S_Y:
-            tmpB = 0x01;
-            PORTB = tmpB;
-            break;
-		default:
-			break;
-    }
-}
+		case unlock:
+			if (PINA == 0x03) { //pressing "#" first
+				PINA = 0x00; //pinA is released
+		                if (PINA == 0x02) { //pressing "Y" now
+					state = door_state;
+					tmpA = 0x01; //PORTC is set to 1 to unlock
+				}
+				else {
+					state = door_state;
+					tmpA = 0x00;
+				}
+			}
 
+			break;
+		case lock: 
+			if (PINA == 0x07) {
+				state = door_state;
+				tmpA = 0x00;
+			}
+		default:
+		{
+			break;}
+	}
+	switch(state) {
+		case Start:
+		{       PORTC = 0x00;
+			break;
+		}
+		case door_state:
+			PORTC = tmpA;
+			
+			break;
+		default:
+		{
+			break;}
+	}
+}
 int main() {
-    state = START;
-    DDRA = 0x00; PORTA = 0xFF;
-    DDRB = 0xFF; PORTB = 0x00;
-    while(1) {
-        tmpA = PINA & 0x87;
-        Tick();
+    /* Insert DDR and PORT initializations */
+	state = Start;
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+
+    /* Insert your solution below */
+    while (1) {
+
+	Tick();
     }
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-    
-
-
-
-		
