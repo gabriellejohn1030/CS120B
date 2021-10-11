bd06e65a2f115de89059881a9d7f298af00a6307
Gabrielle John <gjohn010@wch136-24.cs.ucr.edu>
2021-10-10 20:46:24 -0700

    completed lab 4 part 2

--- Lab4_StateMachines/turnin/gjohn010_lab4_part2.c
+++ Lab4_StateMachines/turnin/gjohn010_lab4_part2.c
@@ -1,7 +1,7 @@
 /*	Author: gjohn010
  *  Partner(s) Name: 
- *	Lab Section: 023
- *	Assignment: Lab #4  Exercise #1
+ *	Lab Section: 022
+ *	Assignment: Lab #4  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -12,198 +12,69 @@
 #include "simAVRHeader.h"
 #endif
 
-/*
-enum my_states { start, s0, s1 } my_state;
+enum States { Start, PA0, PA1, reset } state;
+unsigned char tmp1 = 0x00;
+unsigned char tmp2 = 0x00;
+unsigned char output = 7;
 
-void tick() {
-	switch (my_state) { //transitions
-		case start:
-			my_state = s0;		
+void Tick() {
+	switch(state) {
+		case Start:
+			state = Start;
 			break;
-		case s0:
-			if(!PINA) {
-				my_state = s0;
-			}
-			else {
-				my_state = s1;
-			}
-			break;
-		case s1:
-			if(!PINA) {
-				my_state = s0;
-			}
-			else {
-				my_state = s1;
-			}
-			break;
-		default:
-			my_state = start;
-			break;
-	}
-	
-	switch (my_state) {
-		case s0:
-			PORTB = 0x01;
-			break;
-		case s1:
-			PORTB = 0x02;
-			break;
-		default:
-			break;
-
-	}
-}
-
-int main(void) {
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-        PORTB = 0x01;	
-	my_state = start;
-
-
-	while(1) {
-		tick();
-	}
-
-
-
-
-	
-return 0;
-}
-*/
-
-enum my_states { start, s0, inc, dec, zero } my_state;
-
-void tick() {
-       switch(my_state) {
-               case start:
-                       my_state = s0;
-                       break;
-               case s0:
+		case PA0:
 			if (PINA == 0x01) {
-				my_state = inc;
-			}
-			else if (PINA == 0x02) {
-				my_state = dec;
-			}
-			else if (PINA == 0x03) {
-				my_state = zero;
+				state = PA0 ;
+			
 			}
 			break;
-		case inc:
-			my_state = s0;
+		case PA1: 
+			if (PINA == 0x02) {
+				state = PA1;
+			}
 			break;
-		case dec:
-			my_state = s0;
-			break;
-		case zero:
-			my_state = s0;
-			break;
+		case reset:
+			if (PINA == 0x01 && PINA == 0x02) {
+				state = reset;
+			}
 		default:
-			my_state = start;
-			break;
+		{
+			break;}
 	}
-
-	switch(my_state) {
-		case start:
-			PORTC = 0x07;
+	switch(state) {
+		case Start:
+		{       PORTC = 0x07;
 			break;
-		case s0:
-			break;
-		case inc:
-			if (PORTC < 9){
-				PORTC++;	
+		}
+		case PA0:	
+			if (PORTC < 0x09){
+				PORTC++;
+				
 			}
 			break;
-		case dec:
+		case PA1:
 			if (PORTC > 0x00) {
-				PORTC = PORTC - 0x01;
+				PORTC--;
+			
 			}
 			break;
-		case zero:
-			PORTC = 0x00;
-			break;
+		case reset:
+			PORTC = 0x00;	
 		default:
-			break;
+		{
+			break;}
 	}
 }
-
-int main(void) {
+int main() {
+    /* Insert DDR and PORT initializations */
+	state = Start;
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	
-	my_state = start;
-	PORTC = 0x07;
+	DDRC = 0xFF; PORTC = 0x00;
 
-	while(1) {
-		tick();
-	}
+    /* Insert your solution below */
+    while (1) {
 
-
-
-
-	
-return 0;
+	Tick();
+    }
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
