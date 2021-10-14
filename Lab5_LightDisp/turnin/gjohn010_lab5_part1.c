d409437e1f36bc563d39bc98300439b5e6c6dfd7
Gabrielle John <gjohn010@wch136-24.cs.ucr.edu>
2021-10-14 15:37:23 -0700

    modified part 1

--- Lab5_LightDisp/turnin/gjohn010_lab5_part1.c
+++ Lab5_LightDisp/turnin/gjohn010_lab5_part1.c
@@ -13,13 +13,6 @@
 #endif
 
 
-
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
-
-
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
